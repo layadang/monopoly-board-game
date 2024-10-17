08dc6905047cd3144223b162c3bb2df9dc482c5f
Laya Dang <pd03@bu.edu>
2024-10-17 15:11:34 -0400

    function outlines for board and player object

---
+++ board.py
@@ -0,0 +1,12 @@
+# MONOPOLY BOARD OBJECT
+
+# functions needed:
+#   init with properties (dictionary with rent) and index (array) and bidding strategy
+#   setup_board() that loads in all properties information
+#   get_property_at() that returns the rent price of certain index
+#   auction() that simulations an auction, takes in property,  player object, and bidding strategy
+
+
+class Board:
+    def __init__(self):
+        return 0
---
+++ player.py
@@ -0,0 +1,11 @@
+# MONOPOLY PLAYER OBJECT
+
+# functions needed:
+#   init with wealth (int), owned_properties (array), position on board (index), bankruptcy (boolean)
+#   move() that randomly rolls dice (selects 1-12) and increases position on board
+#   pay_rent() that decreases rent from wealth
+#   add_property() that adds property information to owned_properties array
+
+class Player:
+    def __init__(self):
+        return 0
