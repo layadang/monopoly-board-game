08987fb66cf367844a5a014e8e6ce7664eb66393
Gabi Guillermo <guillermo.gabe@gmail.com>
2024-10-30 13:00:15 -0400

    added functions for player class, edited requirements to add numpy

--- player.py
+++ player.py
@@ -6,7 +6,45 @@
 #   pay_rent() that decreases rent from wealth
 #   add_property() that adds property information to owned_properties array
 
+import numpy as np
+
 class Player:
     def __init__(self, i):
         # Player number as 1 or 2
-        self.i = i 
+        self.i = i
+        
+        # Wealth
+        self.wealth = 1500
+        
+        # Owned properties
+        self.owned_properties = []
+        
+        # Position on board
+        self.position = 0
+        
+        # Bankruptcy status
+        self.bankruptcy = False
+        
+        # Auction strategy
+        self.auction_strategy = None
+        
+    def move(self):
+        """
+            Randomly rolls dice (1-12) and moves player position
+        """
+        roll = np.random.randint(1, 13)
+        self.position += roll
+        if(self.position >= 40):
+            self.position -= 40
+            
+    def pay_rent(self, rent):
+        """
+            Decrease rent from wealth
+        """
+        self.wealth -= rent
+        
+    def add_property(self, property):
+        """
+            Add property to owned_properties
+        """
+        self.owned_properties.append(property)
